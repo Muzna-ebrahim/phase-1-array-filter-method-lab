c37cb0124c833741707b5b2953e76d6122f18ea3
muzna <ebrahimmuznah98@gmail.com>
2025-06-20 02:14:46 +0300

    names matching

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching (drivers, Name) {
+    return drivers.filter(driver => driver.toLowerCase() === Name.toLowerCase());
+}
+
+function fuzzyMatch (drivers, names) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(names.toLowerCase()));
+}
+
+function matchName (drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
